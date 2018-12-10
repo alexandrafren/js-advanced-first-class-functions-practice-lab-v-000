4e1ed348f3ab1e46dce10c7cfdd4a66ae36aaab3
Alexandra Fren <alexandra.fren@gmail.com>
2018-12-10 23:04:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Code your solution in this file!
+function logDriverNames(drivers) {
+  const callback = function(el, i, arr) {
+    console.log(el);
+  }
+  return drivers.forEach(function(driver){ console.log(driver.name);});
+}
